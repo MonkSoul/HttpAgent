8209ae193d121242bce03619ef264f1b44479def
MonkSoul <monksoul@outlook.com>
2025-02-08 10:25:45 +0800

    😊 添加控制台使用 `HttpAgent` 示例

---
+++ samples/HttpAgent.Console.Samples/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿// global using 指令
+
+global using HttpAgent;
+global using Microsoft.Extensions.DependencyInjection;
---
+++ samples/HttpAgent.Console.Samples/Program.cs
@@ -0,0 +1,12 @@
+﻿var services = new ServiceCollection();
+
+// 注册 HTTP 远程请求服务
+services.AddHttpRemote();
+
+// 构建 IServiceProvider 实例并获取 IHttpRemoteService 服务实例
+await using var serviceProvider = services.BuildServiceProvider();
+var httpRemoteService = serviceProvider.GetRequiredService<IHttpRemoteService>();
+
+// 使用
+var content = await httpRemoteService.GetAsStringAsync("https://furion.net");
+Console.WriteLine(content);
